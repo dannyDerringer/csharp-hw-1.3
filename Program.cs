0d0726313fbb5afb45291caeda92b85ac5612687
Danny Derringer <danny.derringer@gmail.com>
2017-05-10 19:48:10 -0400

    Max Value
    Write a function called Max that accepts a variable number of integer
    parameters. Find the largest number in the collection and return it from
    your Max function.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace csharp_hw_1._3
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List <int> numbers = new List<int> {7, 11, 111, 311, 411, 
+            711, 911};
+
+            int max = numbers[0];
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                max = Math.Max(max, numbers[i]);
+            }
+
+            Console.WriteLine("Current max = " + max);
         }
+
     }
 }
